e131e880d77836dc5413d8d0a61817dc876443fb
yugal1005 <yugalpanchal01@gmail.com>
2022-08-15 16:03:45 +0530

    deleted unnecessary files

--- lib/data/quiz_display_data.dart
+++
@@ -1,6 +0,0 @@
-class QuizDisplayData {
-  List<String> grade = [];
-  List<String> subject = [];
-  List<String> topic = [];
-  List<String> difficulty = [];
-}
--- lib/data/quiz_searching_data.dart
+++
@@ -1,9 +0,0 @@
-class QuizSearchData {
-  QuizSearchData(List<String> tempGrade) {
-    grade = [...tempGrade];
-  }
-  List<String> grade = [];
-  List<String> subject = [];
-  List<String> topic = [];
-  List<String> difficulty = [];
-}
